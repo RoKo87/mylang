6087705ea732e2462c809125b6aba44d1d0938a0
Rohan Konda <78698358+RoKo87@users.noreply.github.com>
2024-02-22 12:23:41 -0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,13 +2,12 @@
 import Parser from "./front/parser.ts";
 import Environment, { gscope } from "./runtime/environment.ts";
 import {evaluate} from "./runtime/interpreter.ts";
-import { INull, INum, IBool } from "./runtime/value.ts";
 
 repl();
-
-async function repl () {
+//may need to be async
+function repl () {
     const parser = new Parser();
-    const env = gscope();
+    const env : Environment = gscope();
     console.log("\nLet's go!");
     while (true) {
         const input = prompt(" ");
